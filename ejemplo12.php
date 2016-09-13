b1d311306842a50b4fbe92bd91b3f603f44f19cb
jordimassa <jordimassa@hotmail.es>
2016-09-13 18:38:07 +0200

    Subimos ejemplo 12 (Switch)

---
+++ ejemplo12.php
@@ -0,0 +1,27 @@
+ <?php
+$n_dia=date("N");
+echo $n_dia." ";
+switch ($n_dia) {
+    case 1:
+        echo "Lunes";
+        break;
+    case 2:
+        echo "Martes";
+        break;
+    case 3:
+        echo "Miércoles";
+        break;
+    case 4:
+        echo "Jueves";
+        break;
+    case 5:
+        echo "Viernes";
+        break;
+    case 6:
+        echo "Sábado";
+        break;
+    case 7:
+        echo "Domingo";
+        break;
+    }
+?> 
